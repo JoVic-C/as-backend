a6ce2304233d8cd337755f897f659c8b3c2216e3
JoVic-C <jo.joaovictor828@gmail.com>
2024-01-23 20:21:21 -0300

    Finalização de Projeto Amigo Secreto  utilizando Node.js

---
+++ src/controllers/auth.ts
@@ -0,0 +1,35 @@
+import { RequestHandler } from "express";
+import { z } from "zod";
+import * as auth from '../services/auth';
+
+export const login: RequestHandler = (req, res) => {
+    const loginSchema = z.object({
+        password: z.string()
+    });
+    const body = loginSchema.safeParse(req.body);
+    if(!body.success) return res.json({ error: 'Dados inválidos' })
+
+    //validar a senha & gerar token
+    if(!auth.validatePassword(body.data.password)) {
+        res.status(403).json({ error: 'Acesso negado' });
+    }
+    
+    res.json({ token: auth.createToken() });
+    
+
+    //retorno da requisição
+}
+
+export const validate: RequestHandler = (req, res, next) => {
+    if(!req.headers.authorization) {
+        return res.status(403).json({ error: 'Acesso negado' });
+    }
+
+    const token =  req.headers.authorization.split(' ')[1];
+    if(!auth.validateToken(token)) {
+        return res.status(403).json({ error: 'Acesso negado' });
+    }
+
+
+    next();
+}
